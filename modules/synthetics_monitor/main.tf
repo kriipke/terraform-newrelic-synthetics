2ef013612a59463a7129f5ce5b1f67a2e34f243a
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2025-07-01 13:20:52 -0400

    Update main.tf

--- modules/synthetics_monitor/main.tf
+++ modules/synthetics_monitor/main.tf
@@ -16,12 +16,19 @@ resource "newrelic_synthetics_monitor" "this" {
   frequency            = 10
   status               = "ENABLED"
   uri                  = "https://${each.value.url}/version"
-  locations_public     = []
-  locations_private    = [newrelic_synthetics_private_location.this[0].name]
+  locations_public     = each.value.locations_public
+  locations_private    = (
+    length(each.value.locations_private) > 0
+      ? each.value.locations_private
+      : (
+          var.create_private_location
+            ? [newrelic_synthetics_private_location.this[0].name]
+            : []
+        )
+  )
   sla_threshold        = 7.0
 
-  script               = <<EOT
-// $browser is not needed for SIMPLE type
+  script = <<EOT
 $http.get("${each.value.url}/version", {
   headers: {
     "User-Agent": "NR-Synthetics-TF"
@@ -33,18 +40,18 @@ EOT
 resource "newrelic_synthetics_alert_condition" "this" {
   for_each = newrelic_synthetics_monitor.this
 
-  policy_id             = var.alert_policy_id
-  name                  = "Synthetics Failure - ${each.value.name}"
-  monitor_id            = each.value.id
-  runbook_url           = var.runbook_url
-  enabled               = true
+  policy_id                   = var.alert_policy_id
+  name                        = "Synthetics Failure - ${each.value.name}"
+  monitor_id                  = each.value.id
+  runbook_url                 = var.runbook_url
+  enabled                     = true
   violation_time_limit_seconds = 3600
 
   terms {
-    threshold      = "1"
-    threshold_duration = 300
-    threshold_occurrences = "ALL"
-    operator       = "equal"
-    priority       = "CRITICAL"
+    threshold              = "1"
+    threshold_duration     = 300
+    threshold_occurrences  = "ALL"
+    operator               = "equal"
+    priority               = "CRITICAL"
   }
 }
