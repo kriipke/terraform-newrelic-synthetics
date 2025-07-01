fcb06eaf5ee8750c8e33f74e424c0cdb210f15b6
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2025-07-01 15:33:52 -0400

    Update main.tf

--- modules/synthetics_monitor/main.tf
+++ modules/synthetics_monitor/main.tf
@@ -5,28 +5,32 @@ resource "newrelic_synthetics_private_location" "this" {
   enabled      = true
   account_id   = var.account_id
 }
-
 resource "newrelic_synthetics_monitor" "this" {
   for_each = {
     for service in var.services : service.name => service
   }
 
-  name                 = "${each.value.name} /version"
-  type                 = "SIMPLE"
-  frequency            = 10
-  status               = "ENABLED"
-  uri                  = "https://${each.value.url}/version"
-  locations_public     = each.value.locations_public
-  locations_private    = (
-    length(each.value.locations_private) > 0
-      ? each.value.locations_private
-      : (
-          var.create_private_location
-            ? [newrelic_synthetics_private_location.this[0].name]
-            : []
-        )
-  )
-  sla_threshold        = 7.0
+  name       = "${each.value.name} /version"
+  type       = "SIMPLE"
+  frequency  = 10
+  status     = "ENABLED"
+  uri        = "https://${each.value.url}/version"
+  sla_threshold = 7.0
+
+  dynamic "locations_public" {
+    for_each = length(each.value.locations_public) > 0 ? [each.value.locations_public] : []
+    content {
+      locations_public = locations_public.value
+    }
+  }
+
+  dynamic "locations_private" {
+    for_each = length(each.value.locations_private) > 0 ? [each.value.locations_private] :
+      (var.create_private_location ? [[newrelic_synthetics_private_location.this[0].name]] : [])
+    content {
+      locations_private = locations_private.value
+    }
+  }
 
   script = <<EOT
 $http.get("${each.value.url}/version", {
@@ -36,6 +40,8 @@ $http.get("${each.value.url}/version", {
 });
 EOT
 }
+
+
 
 resource "newrelic_nrql_alert_condition" "this" {
   for_each = newrelic_synthetics_monitor.this
